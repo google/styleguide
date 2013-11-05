0075d1469a7b1aca881f7750c526691279a20db8
erg@google.com <erg@google.com@955884f1-7149-0410-9467-59e7ac3f4d80>
2013-11-05 22:28:07 +0000

    A false positive occurs when a user specifies a using declaration for a stream operator:
        10: using ns::operator<<;
        file.cpp:10: Missing spaces around << [whitespace/operators] [3]
    
    The regular expression has been updated to find this valid use case of the <<
    text string.
    
    Review URL: https://codereview.appspot.com/22190043
    
    Patch from Matt Clarkson <mattyclarkson@gmail.com>.

--- cpplint/cpplint.py
+++ cpplint/cpplint.py
@@ -2681,8 +2681,11 @@ def CheckSpacing(filename, clean_lines, linenum, nesting_state, error):
           'Missing spaces around %s' % match.group(1))
   # We allow no-spaces around << when used like this: 10<<20, but
   # not otherwise (particularly, not when used as streams)
-  match = Search(r'(\S)(?:L|UL|ULL|l|ul|ull)?<<(\S)', line)
-  if match and not (match.group(1).isdigit() and match.group(2).isdigit()):
+  # Also ignore using ns::operator<<;
+  match = Search(r'(operator|\S)(?:L|UL|ULL|l|ul|ull)?<<(\S)', line)
+  if (match and
+      not (match.group(1).isdigit() and match.group(2).isdigit()) and
+      not (match.group(1) == 'operator' and match.group(2) == ';')):
     error(filename, linenum, 'whitespace/operators', 3,
           'Missing spaces around <<')
   elif not Match(r'#.*include', line):
