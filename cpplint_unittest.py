e54af920ff26f92b28f95e5ada79262811e3229b
Andrew Davis <theandrewdavis@gmail.com>
2016-01-15 20:58:31 -0500

    Fixed some 4 space indents

--- cpplint_unittest.py
+++ cpplint_unittest.py
@@ -4201,79 +4201,79 @@ class CpplintTest(CpplintTestBase):
   def testBuildHeaderGuardWithRoot(self):
     temp_directory = tempfile.mkdtemp()
     try:
-        os.makedirs(os.path.join(temp_directory, ".svn"))
-        header_directory = os.path.join(temp_directory, "cpplint")
-        os.makedirs(header_directory)
-        file_path = os.path.join(header_directory, 'cpplint_test_header.h')
-        open(file_path, 'a').close()
-        file_info = cpplint.FileInfo(file_path)
-        if file_info.FullName() == file_info.RepositoryName():
-            # When FileInfo cannot deduce the root directory of the repository,
-            # FileInfo.RepositoryName returns the same value as FileInfo.FullName.
-            # This can happen when this source file was obtained without .svn or
-            # .git directory. (e.g. using 'svn export' or 'git archive').
-            # Skip this test in such a case because --root flag makes sense only
-            # when the root directory of the repository is properly deduced.
-            return
+      os.makedirs(os.path.join(temp_directory, ".svn"))
+      header_directory = os.path.join(temp_directory, "cpplint")
+      os.makedirs(header_directory)
+      file_path = os.path.join(header_directory, 'cpplint_test_header.h')
+      open(file_path, 'a').close()
+      file_info = cpplint.FileInfo(file_path)
+      if file_info.FullName() == file_info.RepositoryName():
+        # When FileInfo cannot deduce the root directory of the repository,
+        # FileInfo.RepositoryName returns the same value as FileInfo.FullName.
+        # This can happen when this source file was obtained without .svn or
+        # .git directory. (e.g. using 'svn export' or 'git archive').
+        # Skip this test in such a case because --root flag makes sense only
+        # when the root directory of the repository is properly deduced.
+        return
 
-        self.assertEqual('CPPLINT_CPPLINT_TEST_HEADER_H_',
-                          cpplint.GetHeaderGuardCPPVariable(file_path))
-        cpplint._root = 'cpplint'
-        self.assertEqual('CPPLINT_TEST_HEADER_H_',
-                          cpplint.GetHeaderGuardCPPVariable(file_path))
-        # --root flag is ignored if an non-existent directory is specified.
-        cpplint._root = 'NON_EXISTENT_DIR'
-        self.assertEqual('CPPLINT_CPPLINT_TEST_HEADER_H_',
-                          cpplint.GetHeaderGuardCPPVariable(file_path))
+      self.assertEqual('CPPLINT_CPPLINT_TEST_HEADER_H_',
+                        cpplint.GetHeaderGuardCPPVariable(file_path))
+      cpplint._root = 'cpplint'
+      self.assertEqual('CPPLINT_TEST_HEADER_H_',
+                        cpplint.GetHeaderGuardCPPVariable(file_path))
+      # --root flag is ignored if an non-existent directory is specified.
+      cpplint._root = 'NON_EXISTENT_DIR'
+      self.assertEqual('CPPLINT_CPPLINT_TEST_HEADER_H_',
+                        cpplint.GetHeaderGuardCPPVariable(file_path))
     finally:
-        shutil.rmtree(temp_directory)
+      shutil.rmtree(temp_directory)
 
   def testBuildHeaderGuardWithRepository(self):
     temp_directory = tempfile.mkdtemp()
     temp_directory2 = tempfile.mkdtemp()
     try:
-        os.makedirs(os.path.join(temp_directory, ".svn"))
-        trunk_dir = os.path.join(temp_directory, "trunk")
-        os.makedirs(trunk_dir)
-        header_directory = os.path.join(trunk_dir, "cpplint")
-        os.makedirs(header_directory)
-        file_path = os.path.join(header_directory, 'cpplint_test_header.h')
-        open(file_path, 'a').close()
-        file_info = cpplint.FileInfo(file_path)
+      os.makedirs(os.path.join(temp_directory, ".svn"))
+      trunk_dir = os.path.join(temp_directory, "trunk")
+      os.makedirs(trunk_dir)
+      header_directory = os.path.join(trunk_dir, "cpplint")
+      os.makedirs(header_directory)
+      file_path = os.path.join(header_directory, 'cpplint_test_header.h')
+      open(file_path, 'a').close()
+      file_info = cpplint.FileInfo(file_path)
 
-        # search for .svn if _repository is not specified
-        self.assertEqual('TRUNK_CPPLINT_CPPLINT_TEST_HEADER_H_',
-                          cpplint.GetHeaderGuardCPPVariable(file_path))
+      # search for .svn if _repository is not specified
+      self.assertEqual('TRUNK_CPPLINT_CPPLINT_TEST_HEADER_H_',
+                        cpplint.GetHeaderGuardCPPVariable(file_path))
 
-        # use the provided repository root for header guards
-        cpplint._repository = os.path.relpath(trunk_dir)
-        self.assertEqual('CPPLINT_CPPLINT_TEST_HEADER_H_',
-                          cpplint.GetHeaderGuardCPPVariable(file_path))
-        cpplint._repository = os.path.abspath(trunk_dir)
-        self.assertEqual('CPPLINT_CPPLINT_TEST_HEADER_H_',
-                          cpplint.GetHeaderGuardCPPVariable(file_path))
+      # use the provided repository root for header guards
+      cpplint._repository = os.path.relpath(trunk_dir)
+      self.assertEqual('CPPLINT_CPPLINT_TEST_HEADER_H_',
+                        cpplint.GetHeaderGuardCPPVariable(file_path))
+      cpplint._repository = os.path.abspath(trunk_dir)
+      self.assertEqual('CPPLINT_CPPLINT_TEST_HEADER_H_',
+                        cpplint.GetHeaderGuardCPPVariable(file_path))
 
-        # ignore _repository if it doesnt exist
-        cpplint._repository = os.path.join(temp_directory, 'NON_EXISTANT')
-        self.assertEqual('TRUNK_CPPLINT_CPPLINT_TEST_HEADER_H_',
-                          cpplint.GetHeaderGuardCPPVariable(file_path))
+      # ignore _repository if it doesnt exist
+      cpplint._repository = os.path.join(temp_directory, 'NON_EXISTANT')
+      self.assertEqual('TRUNK_CPPLINT_CPPLINT_TEST_HEADER_H_',
+                        cpplint.GetHeaderGuardCPPVariable(file_path))
 
-        # ignore _repository if it exists but file isn't in it
-        cpplint._repository = os.path.relpath(temp_directory2)
-        self.assertEqual('TRUNK_CPPLINT_CPPLINT_TEST_HEADER_H_',
-                          cpplint.GetHeaderGuardCPPVariable(file_path))
+      # ignore _repository if it exists but file isn't in it
+      cpplint._repository = os.path.relpath(temp_directory2)
+      self.assertEqual('TRUNK_CPPLINT_CPPLINT_TEST_HEADER_H_',
+                        cpplint.GetHeaderGuardCPPVariable(file_path))
 
-        # _root should be relative to _repository
-        cpplint._repository = os.path.relpath(trunk_dir)
-        cpplint._root = 'cpplint'
-        self.assertEqual('CPPLINT_TEST_HEADER_H_',
-                          cpplint.GetHeaderGuardCPPVariable(file_path))
+      # _root should be relative to _repository
+      cpplint._repository = os.path.relpath(trunk_dir)
+      cpplint._root = 'cpplint'
+      self.assertEqual('CPPLINT_TEST_HEADER_H_',
+                        cpplint.GetHeaderGuardCPPVariable(file_path))
 
     finally:
-        shutil.rmtree(temp_directory)
-        shutil.rmtree(temp_directory2)
-        cpplint._repository = None
-        cpplint._root = None
+      shutil.rmtree(temp_directory)
+      shutil.rmtree(temp_directory2)
+      cpplint._repository = None
+      cpplint._root = None
 
   def testBuildInclude(self):
     # Test that include statements have slashes in them.
