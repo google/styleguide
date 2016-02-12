2859186abfdccd4143c16167d36075fc9d83daec
Andrew Davis <theandrewdavis@gmail.com>
2016-02-11 21:05:42 -0500

    Fix #2: sync PyPi README with GitHub readme

--- setup.py
+++ setup.py
@@ -22,12 +22,4 @@ setup(name='cpplint',
                    "Programming Language :: C++",
                    "License :: Freely Distributable"],
       description="An automated checker to make sure a C++ file follows Google's C++ style guide",
-      long_description="""This is an automated checker to make sure a C++ file follows Google's C++ style
-guide (https://google.github.io/styleguide/cppguide.html). As it heavily relies
-on regular expressions, cpplint.py won't catch all violations of the style
-guide and will very occasionally report a false positive. There is a list of
-things we currently don't handle very well at the top of cpplint.py, and we
-welcome patches to improve it.
-
-This version includes some fixes and additional features. For the original
-Google version, see https://github.com/google/styleguide/.""")
+      long_description=open('README.rst').read())
