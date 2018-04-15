475a4596f8c1b333ff2531308d740fe31f949f3b
284km <k.furuhashi10@gmail.com>
2018-04-15 22:02:40 +0900

    Fix a typo in cpplint
    s/seperated/separated

--- cpplint.py
+++ cpplint.py
@@ -685,7 +685,7 @@ def ProcessHppHeadersOption(val):
     # Automatically append to extensions list so it does not have to be set 2 times
     _valid_extensions.update(_hpp_headers)
   except ValueError:
-    PrintUsage('Header extensions must be comma seperated list.')
+    PrintUsage('Header extensions must be comma separated list.')
 
 def IsHeaderExtension(file_extension):
   return file_extension in _hpp_headers
